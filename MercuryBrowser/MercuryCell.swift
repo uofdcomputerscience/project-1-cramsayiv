c83d336fe648efd96103a19fa64936705f22305c
cramsayiv <37635288+cramsayiv@users.noreply.github.com>
2019-11-29 14:36:57 -0600

    finish project

--- MercuryBrowser/MercuryCell.swift
+++ MercuryBrowser/MercuryCell.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import UIKit
+
+class MercuryCell: UITableViewCell {
+
+    @IBOutlet weak var mercuryImage: UIImageView!
+    @IBOutlet weak var mercuryName: UILabel!
+    @IBOutlet weak var mercuryType: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
